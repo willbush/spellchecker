326f35fb344697b8fa6d195bcb5ba468ceebe6e6
willbush <william.bush@utdallas.edu>
2015-02-22 05:37:48 -0600

    improve insert and listAll still does not work for various edge cases.

--- src/WIBUP2.java
+++ src/WIBUP2.java
@@ -1,98 +1,114 @@
 class Node {
-	protected static final int ALPHABET_SIZE = 26;
-	int outDegree;
-	boolean terminal;
-	Children[] node;
+    protected static final int ALPHABET_SIZE = 26;
+    int outDegree;
+    boolean terminal;
+    Children[] node;
 
-	Node() {
-		outDegree = 0;
-		terminal = false;
-		node = new Children[ALPHABET_SIZE];
-	}
+    Node() {
+        outDegree = 0;
+        terminal = false;
+        node = new Children[ALPHABET_SIZE];
+    }
 }
 
 class Children extends Node {
-	Children(int outDegree, boolean terminal) {
-		this.outDegree = outDegree;
-		this.terminal = terminal;
-		node = new Children[ALPHABET_SIZE];
-	}
+    Children(int outDegree, boolean terminal) {
+        this.outDegree = outDegree;
+        this.terminal = terminal;
+        node = new Children[ALPHABET_SIZE];
+    }
 }
 
 class Trie {
-	Node head;
+    Node head;
 
-	Trie() {
-		head = new Node();
-	}
+    Trie() {
+        head = new Node();
+    }
 
-	public boolean insert(String word) {
-		return insert(word, head);
-	}
+    public boolean insert(String word) {
+        return insert(word, head);
+    }
 
-	private boolean insert(String word, Node x) {
-		if (word.length() == 0) {
-			return true;
-		}
+    private boolean insert(String word, Node x) {
+        if (word.length() == 1) {
+            int i = getFirstLetterIndex(word);
+            x.node[i] = new Children(1, true);
+            return true;
+        }
 
-		int firstLetterIndex = word.charAt(0) - 97; // a~z == 0~25
-		String suffix = word.substring(1);
-		if (x.node[firstLetterIndex] == null) {
-			x.node[firstLetterIndex] = new Children(1, true);
-			return insert(suffix, x.node[firstLetterIndex]);
-		} else if (x.node[firstLetterIndex] != null) {
-			x.node[firstLetterIndex].outDegree++;
-			return insert(suffix, x.node[firstLetterIndex]);
-		} else
-			return false;
-	}
+        int i = getFirstLetterIndex(word);
+        String suffix = word.substring(1);
+        if (x.node[i] == null) {
+            x.node[i] = new Children(1, false);
+            return insert(suffix, x.node[i]);
+        } else if (x.node[i] != null) {
+            x.node[i].outDegree++;
+            return insert(suffix, x.node[i]);
+        } else
+            return false;
+    }
 
-	public boolean isPresent(String word) {
-		int firstLetterIndex = word.charAt(0) - 97; // a~z == 0~25
-		return head.node[firstLetterIndex] != null;
-	}
+    public boolean isPresent(String word) {
+        int i = getFirstLetterIndex(word);
+        return head.node[i] != null;
+    }
 
-	public boolean delete(String word) {
-		int firstLetterIndex = word.charAt(0) - 97; // a~z == 0~25
-		head.node[firstLetterIndex] = null;
-		return head.node[firstLetterIndex] == null;
-	}
+    public boolean delete(String word) {
+        int i = getFirstLetterIndex(word);
+        head.node[i] = null;
+        return head.node[i] == null;
+    }
 
-	public int membership() {
-		int count = 0;
-		for (Children c : head.node) {
-			if (c != null)
-				count++;
-		}
+    private int getFirstLetterIndex(String word) {
+        return word.charAt(0) - 97;
+    }
 
-		return count;
-	}
+    public int membership() {
+        int count = 0;
+        for (Children c : head.node) {
+            if (c != null)
+                count++;
+        }
 
-	public void listAll() {
-		listAll(head);
-	}
+        return count;
+    }
 
-	private void listAll(Node x) {
-		for (int i = 0; i < 26; i++) {
-			if (isNonTerminalChar(x, i)) {
-				char letter = (char) (97 + i);
-				System.out.print(letter);
-				listAll(x.node[i]);
-			} else if (isTerminalChar(x, i)) {
-				char letter = (char) (97 + i);
-				System.out.println(letter);
-				listAll(x.node[i]);
-			}
-		}
-	}
+    public void listAll() {
+        Node headCopy = head;
+        listAll(headCopy);
+    }
 
-	private boolean isNonTerminalChar(Node x, int i) {
-		return x != null && x.node[i] != null && !x.terminal && i < 26;
-	}
+    private void listAll(Node x) {
+        for (int i = 0; i < 26; i++) {
+            if (isMultipleDegreeChar(x, i)) {
+                System.out.print(getLetter(i));
+                listAll(x.node[i]);
+            } else if (isNonTerminalChar(x, i)) {
+                System.out.print(getLetter(i));
+                listAll(x.node[i]);
+            } else if (isTerminalChar(x, i)) {
+                System.out.println(getLetter(i));
+                listAll(x.node[i]);
+            }
+        }
+    }
 
-	private boolean isTerminalChar(Node x, int i) {
-		return x != null && x.node[i] != null && x.terminal && i < 26;
-	}
+    private boolean isMultipleDegreeChar(Node x, int i) {
+        return isNonTerminalChar(x, i) && x.node[i].outDegree > 1;
+    }
+
+    private boolean isNonTerminalChar(Node x, int i) {
+        return x != null && x.node[i] != null && !x.node[i].terminal;
+    }
+
+    private char getLetter(int i) {
+        return (char) (97 + i);
+    }
+
+    private boolean isTerminalChar(Node x, int i) {
+        return x != null && x.node[i] != null && x.node[i].terminal;
+    }
 }
 
 public class WIBUP2 {
--- test/WIBUP2Test.java
+++ test/WIBUP2Test.java
@@ -9,71 +9,106 @@ import static org.junit.Assert.*;
 
 
 public class WIBUP2Test {
-	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
-	private Trie t;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private Trie t;
 
-	@Before
-	public void arrange() {
-		t = new Trie();
-		System.setOut(new PrintStream(out));
-	}
+    @Before
+    public void arrange() {
+        t = new Trie();
+        System.setOut(new PrintStream(out));
+    }
 
-	@After
-	public void cleanUpOutStream() {
-		System.setOut(null);
-	}
+    @After
+    public void cleanUpOutStream() {
+        System.setOut(null);
+    }
 
-	@Test
-	public void canCreateEmptyNode() {
-		Node n = new Node();
-		assertEquals(n.outDegree, 0);
-		assertFalse(n.terminal);
-		assertEquals(n.node.length, 26);
-		assertNull(n.node[0]); // first element
-		assertNull(n.node[25]); // last element
-	}
+    @Test
+    public void canCreateEmptyNode() {
+        Node n = new Node();
+        assertEquals(n.outDegree, 0);
+        assertFalse(n.terminal);
+        assertEquals(n.node.length, 26);
+        assertNull(n.node[0]); // first element
+        assertNull(n.node[25]); // last element
+    }
 
-	@Test
-	public void canCreateChildNode() {
-		int outDegree = 2;
-		int alphabetSize = 26;
-		Children c = new Children(outDegree, true);
+    @Test
+    public void canCreateChildNode() {
+        int outDegree = 2;
+        int alphabetSize = 26;
+        Children c = new Children(outDegree, true);
 
-		assertEquals(c.outDegree, outDegree);
-		assertTrue(c.terminal);
-		assertEquals(c.node.length, alphabetSize);
-	}
+        assertEquals(c.outDegree, outDegree);
+        assertTrue(c.terminal);
+        assertEquals(c.node.length, alphabetSize);
+    }
 
-	@Test
-	public void canInsertSimpleLetter() {
-		assertTrue(t.insert("a"));
-		assertEquals(1, t.membership());
-		assertTrue(t.insert("z"));
-		assertEquals(2, t.membership());
+    @Test
+    public void canInsertSimpleLetter() {
+        assertTrue(t.insert("a"));
+        assertEquals(1, t.membership());
+        assertTrue(t.insert("z"));
+        assertEquals(2, t.membership());
 
-		assertTrue(t.isPresent("a"));
-		assertTrue(t.isPresent("z"));
+        assertTrue(t.isPresent("a"));
+        assertTrue(t.isPresent("z"));
 
-		assertTrue(t.delete("a"));
-		assertEquals(1, t.membership());
-		assertTrue(t.delete("z"));
-		assertEquals(0, t.membership());
+        assertTrue(t.delete("a"));
+        assertEquals(1, t.membership());
+        assertTrue(t.delete("z"));
+        assertEquals(0, t.membership());
 
-		assertFalse(t.isPresent("a"));
-		assertFalse(t.isPresent("z"));
-	}
+        assertFalse(t.isPresent("a"));
+        assertFalse(t.isPresent("z"));
+    }
 
-	@Test
-	public void canInsertTwoLetterWord() {
-		assertTrue(t.insert("as"));
-		t.listAll();
-		assertEquals("as\n", out.toString());
-		assertEquals(1, t.membership());
-		assertTrue(t.isPresent("as"));
-		assertTrue(t.head.node[0].terminal);
-		assertTrue(t.delete("as"));
-		assertFalse(t.isPresent("as"));
-		assertEquals(0, t.membership());
-		t.listAll();
-	}
+    @Test
+    public void canInsertAndRemoveTwoLetterWord() {
+        assertTrue(t.insert("as"));
+        t.listAll();
+        assertEquals("as\n", out.toString());
+        assertEquals(1, t.membership());
+
+        assertTrue(t.isPresent("as"));
+
+        assertFalse(t.head.node[0].terminal);
+        assertTrue(t.head.node[0].node[getIndex('s')].terminal);
+
+        assertTrue(t.delete("as"));
+        assertFalse(t.isPresent("as"));
+        assertEquals(0, t.membership());
+    }
+
+    @Test
+    public void canInsertTwoWords() {
+        assertTrue(t.insert("tom"));
+        assertTrue(t.head.node[getIndex('t')].node[getIndex('o')].node[getIndex('m')].terminal);
+        assertTrue(t.insert("cat"));
+        t.listAll(); // prints alphabetically
+        assertEquals("cat\ntom\n", out.toString());
+    }
+
+    @Test
+    public void canInsertThreeWords() {
+        assertTrue(t.insert("alice"));
+        assertTrue(t.insert("bob"));
+        assertTrue(t.insert("joe"));
+        t.listAll(); // prints alphabetically
+        assertEquals("alice\nbob\njoe\n", out.toString());
+    }
+
+    @Test
+    public void canInsertThreeSimilarWords() {
+        assertTrue(t.insert("vidi"));
+        assertTrue(t.insert("veni"));
+        assertTrue(t.insert("vici"));
+        assertEquals(t.head.node[getIndex('v')].outDegree, 3);
+        t.listAll(); // prints alphabetically
+        assertEquals("veni\nvici\nvidi", out.toString());
+    }
+
+    private int getIndex(char c) {
+        return c - 97;
+    }
 }
